d3e22cf8df87e45a3008bca4b0aed118f69b9f26
SATSUKIUM <koubeoosakahukuoka@gmail.com>
2026-02-24 14:25:19 +0900

    names of some function changed

--- include/channel_map_simple_item.hpp
+++ include/channel_map_simple_item.hpp
@@ -8,6 +8,7 @@ namespace chmap {
         uint32_t id;
         // id = (ip3rd << 24) | (ip4th << 16) | channel で初期化。channelは最大FFFF
         ChannelMapSimpleItem_FE(uint8_t ip3rd, uint8_t ip4th, uint16_t ch) : id((uint32_t(ip3rd) << 24) | (uint32_t(ip4th) << 16) | uint32_t(ch) ) {}
+        public:
         void decode();
     };
     struct ChannelMapSimpleItem_DET {
