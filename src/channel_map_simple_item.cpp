0a3f45788087ce61fed8276c326b7710e661c394
SATSUKIUM <koubeoosakahukuoka@gmail.com>
2026-03-09 23:54:12 +0900

    hexつけ忘れ

--- src/channel_map_simple_item.cpp
+++ src/channel_map_simple_item.cpp
@@ -5,7 +5,7 @@
 namespace chmap {
     void ChannelMapSimpleItem_DET::decode(){
         // print member variables
-        std::cout << "\tDET name: 0x" << std::setw(8) << std::setfill('0') << name;
+        std::cout << "\tDET name: 0x" << std::hex << std::setw(8) << std::setfill('0') << name;
         std::cout
             << " (in char: "
             << static_cast<char>((name >> 24) & 0xFF)
@@ -13,14 +13,14 @@ namespace chmap {
             << static_cast<char>((name >> 8) & 0xFF)
             << static_cast<char>(name & 0xFF)
             << ")," << std::endl;
-        std::cout << "\tplane: 0x" << std::setw(4) << std::setfill('0') << plane;
+        std::cout << "\tplane: 0x" << std::hex << std::setw(4) << std::setfill('0') << plane;
         std::cout
             << " (in char: "
             << static_cast<char>((plane >> 8) & 0xFF)
             << static_cast<char>(plane & 0xFF)
             << ")," << std::endl;
         std::cout << "\tsegment: " << static_cast<uint8_t>(segment) << std::endl;
-        std::cout << "\tchannel: 0x" << std::setw(8) << std::setfill('0');
+        std::cout << "\tchannel: 0x" << std::hex << std::setw(8) << std::setfill('0');
         std::cout << channel
             << " (in char: "
             << static_cast<char>((channel >> 24) & 0xFF)
