86a72a6b735cfc68bf57ca9ec1ad284a6ab1b400
SATSUKIUM <koubeoosakahukuoka@gmail.com>
2026-03-30 12:23:44 +0900

    エラー対処

--- include/channel_map_simple_item.hpp
+++ include/channel_map_simple_item.hpp
@@ -11,7 +11,7 @@ namespace chmap {
         uint8_t ch{}; // input channel of StrReadout FEE
         ChannelMapSimpleItem_FE(uint8_t ip3rd_, uint8_t ip4th_, uint8_t ch_) :ip3rd(ip3rd_), ip4th(ip4th_), ch(ch_) {}
         public:
-        void decode();
+        void decode() const;
         bool operator<(const ChannelMapSimpleItem_FE& right) const {
             return std::tie(this->ip3rd, this->ip4th, this->ch) < std::tie(right.ip3rd, right.ip4th, right.ch);
         }
@@ -24,7 +24,7 @@ namespace chmap {
         uint32_t channel{};// channel name in 4 char
         ChannelMapSimpleItem_DET(uint32_t name_, uint16_t plane_, uint8_t segment_, uint32_t channel_) : name(name_), plane(plane_), segment(segment_), channel(channel_) {}
         public: 
-        void decode();
+        void decode() const;
         // define operator< for sorting and comparison
         bool operator<(const ChannelMapSimpleItem_DET& right) const {
             return std::tie(this->name, this->plane, this->segment, this->channel) < std::tie(right.name, right.plane, right.segment, right.channel);
