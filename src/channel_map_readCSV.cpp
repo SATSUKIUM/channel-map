281a34ee28eeea379a89196de9b9a20a55273378
SATSUKIUM <koubeoosakahukuoka@gmail.com>
2026-04-04 14:46:57 +0900

    fe側とdet側のitemを作る関数を独立化、再利用したい。の実装

--- src/channel_map_readCSV.cpp
+++ src/channel_map_readCSV.cpp
@@ -175,6 +175,67 @@ namespace chmap {
         return tokens;
     }// std::vector<std::string> ChannelMapDopeness::split_line
 
+    ChannelMapSimpleItem_FE ChannelMapDopeness::makeFEItem(const std::vector<std::string>& tokens) {
+        uint64_t fe_ip_full;
+        uint16_t fe_ip_3rd_4th;
+        uint8_t fe_channel;
+        int fe_magic = 3; // id, channel, data
+        int fe_count = 0;
+        for(int i=0; i<tokens.size(); i++){
+            if(fe_count == 0){
+                fe_ip_full = static_cast<uint64_t>(std::stoull(tokens[i], nullptr, 0));
+                fe_ip_3rd_4th = parse_to16(
+                    std::to_string( (fe_ip_full) & 0xFFFF )
+                );
+            }
+            else if(fe_count == 1){
+                fe_channel = parse_to8(tokens[i]);
+            }
+            fe_count++;
+        }
+        return ChannelMapSimpleItem_FE(fe_ip_3rd_4th >> 8, fe_ip_3rd_4th & 0xFF, fe_channel);
+    } // ChannelMapSimpleItem_FE ChannelMapDopeness::makeFEItem
+
+    ChannelMapSimpleItem_DET ChannelMapDopeness::makeDETItem(const std::vector<std::string>& tokens) {
+        uint8_t det_name_idx;
+        uint8_t det_plane_idx;
+        uint8_t det_segment;
+        uint16_t det_channel_number;
+        uint8_t det_readout_channel_idx;
+        std::string det_name_str;
+        std::string det_plane_str;
+        std::string det_channel_number_str;
+        std::string det_readout_channel_str;
+
+        int det_magic = 6; // id, plane, segment, channel, readout, data
+        int det_count = 0;
+        for(int i=0; i<tokens.size(); i++){
+            if(det_count == 0){
+                det_name_str = tokens[i];
+                if(!detname_dictionary.getIndex(det_name_str, det_name_idx)){
+                    std::cerr << "failed to get index for detector name: " << det_name_str << std::endl;
+                }
+            }else if(det_count == 1){
+                det_plane_str = tokens[i];
+                if(!plane_dictionary.getIndex(det_plane_str, det_plane_idx)){
+                    std::cerr << "failed to get index for detector plane: " << det_plane_str << std::endl;
+                }
+            }else if(det_count == 2){
+                det_segment = parse_to8(tokens[i]);
+            }else if(det_count == 3){
+                det_channel_number_str = tokens[i];
+                det_channel_number = parse_to32(det_channel_number_str);
+            }else if(det_count == 4){
+                det_readout_channel_str = tokens[i];
+                if(!readout_channel_dictionary.getIndex(det_readout_channel_str, det_readout_channel_idx)){
+                    std::cerr << "failed to get index for detector readout channel: " << det_readout_channel_str << std::endl;
+                }
+            }
+            det_count++;
+        }
+        return ChannelMapSimpleItem_DET(det_name_idx, det_plane_idx, det_segment, det_channel_number, det_readout_channel_idx);
+    } // ChannelMapSimpleItem_DET ChannelMapDopeness::makeDETItem
+
     ChannelMapSimpleItem ChannelMapDopeness::makeSimpleItem(const std::vector<std::string>& tokens) {
         int len_tokens = tokens.size();
         uint64_t fe_ip_full;
@@ -200,56 +261,72 @@ namespace chmap {
             std::cout << "  " << t << std::endl;
         }
         #endif
+        std::vector<std::string> fe_tokens;
+        std::vector<std::string> det_tokens;
         for(int i=0; i<len_tokens; ++i) {
-            std::string type = m_element_type[i];
-            if(type == "fe") {
-                // parse front-end related tokens
-                if(fe_count == 0) {
-                    #if DEBUG_PRINT
-                    std::cout << "parsing token(fe_count == 0): " << tokens[i] << " (string)" << std::endl;
-                    std::cout << "This token is interpreted as full FE IP address in uint64_t format " << std::hex << std::stoull(tokens[i], nullptr, 0) << std::dec << std::endl;
-                    #endif
-                    fe_ip_full = static_cast<uint64_t>(std::stoull(tokens[i], nullptr, 0));
-                    fe_ip_3rd_4th = parse_to16(
-                        std::to_string( (fe_ip_full) & 0xFFFF )
-                    );
-                } else if(fe_count == 1) {
-                    #if DEBUG_PRINT
-                    std::cout << "parsing token(fe_count == 1): " << tokens[i] << " (string)" << std::endl;
-                    std::cout << "This token is interpreted as FE channel in uint8_t format " << std::stoul(tokens[i], nullptr, 0) << std::endl;
-                    #endif
-                    fe_channel = parse_to8(tokens[i]);
-                }
+            if(m_element_type[i] == "fe"){
+                fe_tokens.push_back(tokens[i]);
                 fe_count++;
-            } else if(type == "detector") {
-                // parse detector related tokens
-                if(det_count == 0) {
-                    det_name_str = tokens[i];
-                    if(!detname_dictionary.getIndex(det_name_str, det_name_idx)){
-                        std::cerr << "failed to get index for detector name: " << det_name_str << std::endl;
-                    }
-                } else if(det_count == 1) {
-                    det_plane_str = tokens[i];
-                    if(!plane_dictionary.getIndex(det_plane_str, det_plane_idx)){
-                        std::cerr << "failed to get index for detector plane: " << det_plane_str << std::endl;
-                    }
-                } else if(det_count == 2) {
-                    det_segment = parse_to8(tokens[i]);
-                } else if(det_count == 3) {
-                    det_channel_number_str = tokens[i];
-                    det_channel_number = parse_to32(det_channel_number_str);
-                } else if(det_count == 4) {
-                    det_readout_channel_str = tokens[i];
-                    if(!readout_channel_dictionary.getIndex(det_readout_channel_str, det_readout_channel_idx)){
-                        std::cerr << "failed to get index for detector readout channel: " << det_readout_channel_str << std::endl;
-                    }
-                }
+            }
+            else if(m_element_type[i] == "detector"){
+                det_tokens.push_back(tokens[i]);
                 det_count++;
-            } // if type is fe or detector
-        } // for loop for tokens
+            }
+        }
+        ChannelMapSimpleItem_FE fe_item = makeFEItem(fe_tokens);
+        ChannelMapSimpleItem_DET det_item = makeDETItem(det_tokens);
 
-        ChannelMapSimpleItem_FE fe_item(fe_ip_3rd_4th >> 8, fe_ip_3rd_4th & 0xFF, fe_channel);
-        ChannelMapSimpleItem_DET det_item(det_name_idx, det_plane_idx, det_segment, det_channel_number, det_readout_channel_idx);
+
+        // for(int i=0; i<len_tokens; ++i) {
+        //     std::string type = m_element_type[i];
+        //     if(type == "fe") {
+        //         // parse front-end related tokens
+        //         if(fe_count == 0) {
+        //             #if DEBUG_PRINT
+        //             std::cout << "parsing token(fe_count == 0): " << tokens[i] << " (string)" << std::endl;
+        //             std::cout << "This token is interpreted as full FE IP address in uint64_t format " << std::hex << std::stoull(tokens[i], nullptr, 0) << std::dec << std::endl;
+        //             #endif
+        //             fe_ip_full = static_cast<uint64_t>(std::stoull(tokens[i], nullptr, 0));
+        //             fe_ip_3rd_4th = parse_to16(
+        //                 std::to_string( (fe_ip_full) & 0xFFFF )
+        //             );
+        //         } else if(fe_count == 1) {
+        //             #if DEBUG_PRINT
+        //             std::cout << "parsing token(fe_count == 1): " << tokens[i] << " (string)" << std::endl;
+        //             std::cout << "This token is interpreted as FE channel in uint8_t format " << std::stoul(tokens[i], nullptr, 0) << std::endl;
+        //             #endif
+        //             fe_channel = parse_to8(tokens[i]);
+        //         }
+        //         fe_count++;
+        //     } else if(type == "detector") {
+        //         // parse detector related tokens
+        //         if(det_count == 0) {
+        //             det_name_str = tokens[i];
+        //             if(!detname_dictionary.getIndex(det_name_str, det_name_idx)){
+        //                 std::cerr << "failed to get index for detector name: " << det_name_str << std::endl;
+        //             }
+        //         } else if(det_count == 1) {
+        //             det_plane_str = tokens[i];
+        //             if(!plane_dictionary.getIndex(det_plane_str, det_plane_idx)){
+        //                 std::cerr << "failed to get index for detector plane: " << det_plane_str << std::endl;
+        //             }
+        //         } else if(det_count == 2) {
+        //             det_segment = parse_to8(tokens[i]);
+        //         } else if(det_count == 3) {
+        //             det_channel_number_str = tokens[i];
+        //             det_channel_number = parse_to32(det_channel_number_str);
+        //         } else if(det_count == 4) {
+        //             det_readout_channel_str = tokens[i];
+        //             if(!readout_channel_dictionary.getIndex(det_readout_channel_str, det_readout_channel_idx)){
+        //                 std::cerr << "failed to get index for detector readout channel: " << det_readout_channel_str << std::endl;
+        //             }
+        //         }
+        //         det_count++;
+        //     } // if type is fe or detector
+        // } // for loop for tokens
+
+        // ChannelMapSimpleItem_FE fe_item(fe_ip_3rd_4th >> 8, fe_ip_3rd_4th & 0xFF, fe_channel);
+        // ChannelMapSimpleItem_DET det_item(det_name_idx, det_plane_idx, det_segment, det_channel_number, det_readout_channel_idx);
 
         #if DEBUG_PRINT
         std::cout << "constructed ChannelMapSimpleItem_FE: id=" << std::hex << fe_item.id << std::dec << std::endl;
