2ce5abff799aef95e330113b1277121ad7b35859
SATSUKIUM <koubeoosakahukuoka@gmail.com>
2026-04-04 13:18:08 +0900

    index化するのに合わせて引数を変更

--- include/channel_map_simple_item.hpp
+++ include/channel_map_simple_item.hpp
@@ -3,6 +3,10 @@
 
 #include <vector>
 #include <cstdint>
+#include <string>
+#include <string_view>
+#include <vector>
+#include <algorithm>
 
 // for std::tie
 #include <tuple>
@@ -28,23 +32,25 @@ namespace chmap {
     };
     struct ChannelMapSimpleItem_DET {
         // zero initialized default constructor
-        uint32_t name{};// detector name in 4 char
-        uint16_t plane{};// plane name in 2 char
-        uint8_t segment{};// segment number in 8bit int (0-255)
-        uint32_t channel{};// channel name in 4 char
+        uint8_t name{};// indexed detector name(assume 0~63)
+        uint8_t plane{};// indexed detector plane(assume 0~63)
+        uint8_t segment{};// segment number(assume 0~255)
+        uint16_t channel_number{};// channel number(assume 0~1023)
+        uint8_t readout_channel{};// indexed readout channel name(assume 0~255)
         ChannelMapSimpleItem_DET() = default; // default constructor
-        ChannelMapSimpleItem_DET(uint32_t name_, uint16_t plane_, uint8_t segment_, uint32_t channel_) : name(name_), plane(plane_), segment(segment_), channel(channel_) {}
+        ChannelMapSimpleItem_DET(uint8_t name_idx, uint8_t plane_idx, uint8_t segment_, uint16_t channel_number_, uint8_t readout_channel_idx)
+            : name(name_idx), plane(plane_idx), segment(segment_), channel_number(channel_number_), readout_channel(readout_channel_idx) {}
         public: 
         void decode() const;
         // define operator< for sorting and comparison
         bool operator<(const ChannelMapSimpleItem_DET& right) const {
-            return std::tie(this->name, this->plane, this->segment, this->channel) < std::tie(right.name, right.plane, right.segment, right.channel);
+            return std::tie(this->name, this->plane, this->segment, this->channel_number, this->readout_channel) < std::tie(right.name, right.plane, right.segment, right.channel_number, right.readout_channel);
         }
         bool operator==(const ChannelMapSimpleItem_DET& right) const {
-            return std::tie(this->name, this->plane, this->segment, this->channel) == std::tie(right.name, right.plane, right.segment, right.channel);
+            return std::tie(this->name, this->plane, this->segment, this->channel_number, this->readout_channel) == std::tie(right.name, right.plane, right.segment, right.channel_number, right.readout_channel);
         }
-        uint32_t getRawID() const {
-            return (name << 24) | (plane << 8) | (segment << 0) | channel; // segmentは8bitなので、channelと重ならないように0ビットシフト
+        uint32_t getRawID() const { // to be discontenued
+            return (name << 24) | (plane << 8) | (segment << 0) | channel_number; // 使っちゃダメ。壊れます。
         }
     };
     struct ChannelMapSimpleItem {
