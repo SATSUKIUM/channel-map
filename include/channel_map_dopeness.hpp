6b36facbc903206a84b451f6a034bf267562dfc7
SATSUKIUM <koubeoosakahukuoka@gmail.com>
2026-04-06 14:01:53 +0900

    no change

--- include/channel_map_dopeness.hpp
+++ include/channel_map_dopeness.hpp
@@ -30,11 +30,11 @@ namespace chmap {
             void initialize_InvMap();
 
             bool getDopeKey_FEtoDET(uint8_t ip3rd, uint8_t ip4th, uint8_t ch, uint32_t& retKey) const;
-            bool getDopeKey_FEtoDET(const ChannelMapSimpleItem_FE& fe_item, uint32_t& retKey) const { // 既存のものをオーバーロード
+            bool getDopeKey_FEtoDET(const ChannelMapSimpleItem_FE& fe_item, uint32_t& retKey) const { // overload expression
                 return getDopeKey_FEtoDET(fe_item.ip3rd, fe_item.ip4th, fe_item.ch, retKey);
             }
             bool getDopeKey_DETtoFE(uint8_t name_idx, uint8_t plane_idx, uint8_t segment, uint16_t channel_number, uint8_t readout_channel_idx, uint32_t& retKey) const;
-            bool getDopeKey_DETtoFE(const ChannelMapSimpleItem_DET& det_item, uint32_t& retKey) const { // 既存のものをオーバーロード
+            bool getDopeKey_DETtoFE(const ChannelMapSimpleItem_DET& det_item, uint32_t& retKey) const { // overload expression
                 return getDopeKey_DETtoFE(det_item.name, det_item.plane, det_item.segment, det_item.channel_number, det_item.readout_channel, retKey);
             }
 
@@ -43,8 +43,6 @@ namespace chmap {
 
             void printAllItemsFE();
             void printAllItemsDET();
-            // void checkDuplicateFEIDs();
-            // void checkDuplicateFEIDs_summary();
             void printFEid(ChannelMapSimpleItem_FE fe_item);
             void printDETinfo(ChannelMapSimpleItem_DET det_item);
             int getNumberOfChannels() const { return fItems.size(); }
@@ -52,11 +50,10 @@ namespace chmap {
             ChannelMapDopeness(const ChannelMapDopeness&) = delete; // prevent copy constructor
             ChannelMapDopeness& operator=(const ChannelMapDopeness&) = delete; // prevent copy assignment
 
-
-
             class NameIndexDictionary{
                 public:
                     /*
+                    usage:
                     execute newWord(), newWord(), ... in order, then execute sortWords() once, and finally execute buildDictionary() once.
                     */
                     void newWord(const std::string& str); // just adding new word
@@ -74,7 +71,6 @@ namespace chmap {
             NameIndexDictionary plane_dictionary;
             NameIndexDictionary readout_channel_dictionary;
         private:
-
             /*
             FE key: 0x00FFFFFF (ip3rd: 8bit, ip4th: 8bit, ch: 8bit)
             ip3rd, ip4th, chのそれぞれのとりうる値の範囲で張られるdope-vectorの準備
