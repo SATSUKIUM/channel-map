fdb62493a36e739b1278b7853229c41cb38511f7
SATSUKIUM <koubeoosakahukuoka@gmail.com>
2026-07-19 16:18:20 +0900

    ファイル名衝突回避のためディレクトリ構成を変更ush

--- include/chmap/channel_map_dopeness.hpp
+++ include/chmap/channel_map_dopeness.hpp
@@ -7,8 +7,8 @@
 #include <filesystem>
 #include <unordered_map> // only use in initialize, not in DAQ search
 
-#include "item.hpp"
-#include "channel_map_dictionary.hpp"
+#include "chmap/item.hpp"
+#include "chmap/channel_map_dictionary.hpp"
 
 
 namespace chmap {
