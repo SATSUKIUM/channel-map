6448ab8ff0ccfcb9ca2748449f5da13bee8dfec2
SATSUKIUM <koubeoosakahukuoka@gmail.com>
2026-07-25 11:37:50 +0900

    セッターを追加

--- include/chmap/geometry.hpp
+++ include/chmap/geometry.hpp
@@ -7,6 +7,21 @@ namespace chmap {
     class GeomItem{
         public:
             virtual ~GeomItem() = default;
+            void SetGlobalPosition(double x_, double y_, double z_) {
+                globalX = x_;
+                globalY = y_;
+                globalZ = z_;
+            }
+            void SetResolution(double resX_, double resY_, double resZ_) {
+                resolutionX = resX_;
+                resolutionY = resY_;
+                resolutionZ = resZ_;
+            }
+            void SetRotationAngles(double tilt_, double rot1_, double rot2_) {
+                tiltAngle = tilt_;
+                rotAngle1 = rot1_;
+                rotAngle2 = rot2_;
+            }
 
         private:
             uint32_t dopeKey_DET; // getDetItem[dopeKey_DET] return us the corresponding DETIdItem
@@ -19,6 +34,11 @@ namespace chmap {
     class GeomItemDC : public GeomItem {
         public:
             virtual ~GeomItemDC() = default;
+            void SetWireGeometry(double centerWireNumber_, double wirePitch_, double offset_) {
+                centerWireNumber = centerWireNumber_;
+                wirePitch = wirePitch_;
+                offset = offset_;
+            }
         private:
             double centerWireNumber; // もし1.0なら、中心のワイヤーは1番ワイヤー。0.5なら、中心のワイヤーは1番と2番の間にある。
             double wirePitch; // [mm] 測定軸方向のワイヤ間隔
