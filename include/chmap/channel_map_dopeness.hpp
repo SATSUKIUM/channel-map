c1bf4e4d33ba15b2adf3d41e7e97673179c52103
SATSUKIUM <koubeoosakahukuoka@gmail.com>
2026-08-27 10:59:09 +0900

    subitemの登録のオーバーロードを追加

--- include/chmap/channel_map_dopeness.hpp
+++ include/chmap/channel_map_dopeness.hpp
@@ -112,6 +112,7 @@ namespace chmap {
             uint32_t maxDETId; // for out of range handling
             
             std::vector<ItemPair> fItems;
+            
             std::unordered_map<std::string, uint32_t> token_normalization_4char; // for normalization of string tokens to 4char uint32_t
             std::unordered_map<std::string, uint16_t> token_normalization_2char; // for normalization of string tokens to 2char uint16_t
 
